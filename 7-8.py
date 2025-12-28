a89d238ed24e3ee13afa0c1cd933215d17ba182a
justice095 <justicetamon095@gmail.com>
2025-12-28 20:29:41 +0100

    this is my project

---
+++ 7-8.py
@@ -0,0 +1,9 @@
+sandwich_orders = ['tuna', 'chicken', 'veggie', 'beef']
+finished_sandwiches = []
+while sandwich_orders:
+    current_sandwich = sandwich_orders.pop(0)
+    print(f"I made your {current_sandwich} sandwich.")
+    finished_sandwiches.append(current_sandwich)
+print("\nFinished sandwiches:")
+for sandwich in finished_sandwiches:
+    print(f"- {sandwich}")
